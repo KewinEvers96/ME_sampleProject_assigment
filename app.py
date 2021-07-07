6c22297abc7c87f55e9acaa251c6f83db431983b
KewinEvers96 <keversy@unal.edu.co>
2021-07-07 13:24:46 -0500

    Preliminar Changes

--- app.py
+++ app.py
@@ -20,8 +20,14 @@ def getPlayers(height):
         else:
             dict_players[int(player['h_in'])].append("{} {}".format(player['first_name'], player['last_name']))
 
-    
+    height_checked = dict()
+    height_pairs = []
+    counter = 0
     for playerHeight, players in dict_players.items():
+        
+        if playerHeight in height_checked:
+            continue 
+        height_checked[playerHeight] = True
         
         substraction = height - playerHeight
         if substraction < 0:
@@ -29,10 +35,31 @@ def getPlayers(height):
         matchedPlayers = dict_players.get(substraction, False)
         if matchedPlayers is False:
             continue
-        print(players, matchedPlayers)
-
+        height_checked[substraction] = True
+        for p1 in players:
+            for p2 in matchedPlayers:
+                if p1 != p2:
+                    print(p1, p2)
+                    counter +=1
+    
+    if counter == 0:
+        print("Not players found")
+    else:
+        print("Players matched", counter)
 if __name__ == "__main__":
     try:
-        getPlayers(int(sys.argv[1]))
+        number = int(sys.argv[1])
+
+        if number < 0:
+            raise Exception("Negative number entered, please enter a positive value")
+
+        
+        getPlayers(number)
+    except IndexError as ie:
+        print("Please specify a height to begin the query")
+
+    except ValueError as ve:
+        print("Please enter a number as height")
+
     except Exception as e:
         print(e)
